4782df4c4f91bb7f625037f2a8d25ea4efbd5614
gpastukhov <gpastukhov@nic.ru>
2023-11-16 16:24:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# runiti_test
---
+++ test.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings FATAL => 'all';
+use Data::Dumper;
+
+my @paths = (
+    '/api/v1/:storage/:pk/raw',
+    '/api/v1/:storage/:pk/:op',
+    '/api/v1/:storage/:pk',
+);
+
+my $url = 'http://localhost:8080/api/v1/order/123/update';
+my ($paths, $i) = ({}, 0);
+
+for my $path (@paths) {
+    my @arr = split('/', $path);
+    my $y = 0;
+    my @key_indexes = ();
+    for my $elem (@arr) {
+        next if $elem eq '';
+        if ($elem =~ /^:(\S+)/) {
+            $paths->{$i}->{$y} = $elem;
+            $elem = q/(\S+)/;
+            push @key_indexes, $y;
+        }
+        ++$y;
+    }
+    $paths->{$i}->{url} = $path;
+    $paths->{$i}->{re} = join('/', @arr);
+    $paths->{$i}->{key_indexes} = \@key_indexes;
+    ++$i;
+}
+my $compared_index;
+for $i (0..$#paths) {
+    if (my (@params) = $url =~ m/$paths->{$i}->{re}/) {
+        $paths->{$i}->{eq} = $url;
+        for my $j (0..$#params) {
+            $paths->{$i}->{params}->{$paths->{$i}->{$paths->{$i}->{key_indexes}->[$j]}} = $params[$j];
+        }
+        $compared_index = $i;
+        last;
+    }
+}
+print "\nCompared index: $compared_index";
+print "\nCompared params: " . Dumper $paths->{$compared_index};
